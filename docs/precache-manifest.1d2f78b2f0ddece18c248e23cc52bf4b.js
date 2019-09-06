59491d1ad5707d73ea2bfe7b488e549f725e797f
Nikiforov Alexandr <KefirNot@users.noreply.github.com>
2019-09-06 13:35:52 +0700

    docs build

---
+++ docs/precache-manifest.1d2f78b2f0ddece18c248e23cc52bf4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53736be2e56aaebecbacddbe75802b3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23ef4a490498b5c779d9",
+    "url": "/static/css/main.0c907582.chunk.css"
+  },
+  {
+    "revision": "cdd22024665b56543c51",
+    "url": "/static/js/2.99a8ebe5.chunk.js"
+  },
+  {
+    "revision": "23ef4a490498b5c779d9",
+    "url": "/static/js/main.20fa55e4.chunk.js"
+  },
+  {
+    "revision": "3297a99b2f363e80d54c",
+    "url": "/static/js/runtime~main.aee57ced.js"
+  }
+]);
